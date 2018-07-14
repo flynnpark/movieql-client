dab2c46f9224233e141825286cdf671a8fb26990
Flynn <wphestiraid@gmail.com>
2018-07-14 19:50:41 +0900

    Creating a Query with variables

--- src/Detail.js
+++ src/Detail.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Query } from 'react-apollo';
+import { MOVIE_DETAILS } from './queries';
 
 const Detail = ({
   match: {
     params: { movieId }
   }
-}) => {
-  console.log(movieid);
-  return <div>Detail</div>;
-};
+}) => (
+  <Query query={MOVIE_DETAILS} variables={{ movieId }}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading';
+      if (error) return 'Error';
+      return (
+        <React.Fragment>
+          {data.movie.title}
+          {data.movie.medium_cover_image}
+          {data.movie.rating}
+          {data.movie.description_intro}
+        </React.Fragment>
+      );
+    }}
+  </Query>
+);
 
 export default Detail;
